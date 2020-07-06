07bd51549a31c73f9d5917a1192e20da63595a10
vijayramireddy <vijayramireddy543@gmail.com>
2020-07-06 12:58:47 +0530

    "selector types"

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: 'app-servers', element selector
+  //selector: '.app-servers',  clsss selector
+  selector: '[app-servers]', //attribute selector
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
